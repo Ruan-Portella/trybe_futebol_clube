d2a5662ddacf58e32e521092c29fdd9422bc3b62
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 20:10:02 -0300

    Feat: Cria migration Users

---
+++ app/backend/src/database/migrations/02-users.ts
@@ -0,0 +1,34 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import IUsers from '../../Interfaces/IUsers';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUsers>>('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      }
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
