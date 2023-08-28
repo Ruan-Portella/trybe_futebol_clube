561a4c98fb738e4d40775ff2ba338d45db3049c6
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 18:18:25 -0300

    Feat: Configura Routes no app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import Router from './routes';
 
 class App {
   public app: express.Express;
@@ -7,6 +8,7 @@ class App {
     this.app = express();
 
     this.config();
+    this.routes();
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
@@ -22,6 +24,10 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+  }
+
+  private routes(): void {
+    this.app.use(Router);
   }
 
   public start(PORT: string | number): void {
