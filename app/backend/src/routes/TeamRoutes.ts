53544da13f0431a64d0abfaead64491b267c0d54
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 18:25:07 -0300

    Feat: Cria findOne na Route

--- app/backend/src/routes/TeamRoutes.ts
+++ app/backend/src/routes/TeamRoutes.ts
@@ -7,5 +7,6 @@ const teamController = new TeamController();
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => teamController.findAll(req, res));
+router.get('/:id', (req: Request, res: Response) => teamController.findOne(req, res));
 
 export default router;
