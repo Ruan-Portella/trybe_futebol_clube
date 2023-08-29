2921e5ffda78c6e5d7bfbd053f93c760435d986a
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 21:21:24 -0300

    Feat: Cria Verificação de login

--- app/backend/src/service/UserService.ts
+++ app/backend/src/service/UserService.ts
@@ -1,4 +1,5 @@
 import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcryptjs';
 import UserModel from '../model/UserModel';
 import IUserModel from '../Interfaces/IUsersModel';
 
@@ -7,10 +8,15 @@ export default class UserService {
     private userModel: IUserModel = new UserModel(),
   ) {}
 
-  public async findOne(email: string): Promise<unknown> {
+  public async findOne(email: string, password: string): Promise<unknown> {
     const user = await this.userModel.findOne(email);
-    if (!user) return { message: 'User not found' };
+    if (!user) return { status: 401, message: { message: 'Invalid email or password' } };
+    const validatePassword = await bcrypt.compare(password, user.password);
+    if (!validatePassword) {
+      return { status: 401,
+        message: { message: 'Invalid email or password' } };
+    }
     const token = jwt.sign(email, process.env.JWT_SECRET as string);
-    return { message: { token } };
+    return { status: 200, message: { token } };
   }
 }
