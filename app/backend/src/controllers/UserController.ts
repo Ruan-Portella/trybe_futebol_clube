49de381ae23e606814e21f1227b48af454e48a10
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 22:04:25 -0300

    Feat: Cria função findById na Controller

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 import UserService from '../service/UserService';
 
+interface token extends Request {
+  token?: any;
+}
+
 export default class UserController {
   constructor(
     private userService = new UserService(),
@@ -10,6 +14,12 @@ export default class UserController {
     const { email, password } = req.body;
     const user = await this.userService.findOne(email, password) as
     { status: number, message: string };
+    return res.status(user.status).json(user.message);
+  }
+
+  public async findById(req: token, res: Response): Promise<Response> {
+    const { token } = req;
+    const user = await this.userService.findById(token.id) as { status: number, message: string };
     return res.status(user.status).json(user.message);
   }
 }
