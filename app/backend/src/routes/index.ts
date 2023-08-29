118899c46e37a43238aaf36e3ae26da13ab8ee7d
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 21:03:41 -0300

    Feat: Importa rota user

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import TeamRouter from './TeamRoutes';
+import UserRouter from './UserRoutes';
 
 const router = Router();
 
 router.use('/teams', TeamRouter);
+router.use('/login', UserRouter);
 
 export default router;
