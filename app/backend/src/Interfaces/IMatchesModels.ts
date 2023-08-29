0dc687634490fdd6d084a0b978a5e2081203aa80
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 14:47:54 -0300

    Feat: Cria finishMatches na Interface Model

--- app/backend/src/Interfaces/IMatchesModels.ts
+++ app/backend/src/Interfaces/IMatchesModels.ts
@@ -2,6 +2,7 @@ import IMatches from './IMatches';
 
 export interface IMatchesModel {
   findAll(): Promise<IMatches[]>;
+  finishMatchesInProgress(id: number): Promise<boolean>;
 }
 
 export default IMatchesModel;
