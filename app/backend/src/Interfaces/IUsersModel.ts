4af8827a70f13e01a4cc6078bb9e58e3032e4334
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 22:03:48 -0300

    Feat: Adiciona findById na interface

--- app/backend/src/Interfaces/IUsersModel.ts
+++ app/backend/src/Interfaces/IUsersModel.ts
@@ -3,6 +3,7 @@ import IUsers from './IUsers';
 export interface IUsersModel {
   // findAll(): Promise<IUsers[]>;
   findOne(email: string): Promise<IUsers | null>;
+  findById(id: string): Promise<IUsers | null>;
 }
 
 export default IUsersModel;
