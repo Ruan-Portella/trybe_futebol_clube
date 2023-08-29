8a897a0fe499b70714736cf7883789b8bb232901
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 17:40:15 -0300

    Feat: Adiciona atributo updateMatch na interface

--- app/backend/src/Interfaces/IMatchesModels.ts
+++ app/backend/src/Interfaces/IMatchesModels.ts
@@ -3,6 +3,7 @@ import IMatches from './IMatches';
 export interface IMatchesModel {
   findAll(): Promise<IMatches[]>;
   finishMatchesInProgress(id: number): Promise<boolean>;
+  updateMatch(id: number, homeTeam: number, awayTeam: number): Promise<boolean>;
 }
 
 export default IMatchesModel;
