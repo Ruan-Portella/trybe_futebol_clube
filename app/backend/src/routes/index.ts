0a6edde9201039befa36ff3641458558026bfa5f
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 14:14:27 -0300

    Feat: Cria rota /matches

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import TeamRouter from './TeamRoutes';
 import UserRouter from './UserRoutes';
+import MatchesRouter from './MatchesRoutes';
 
 const router = Router();
 
 router.use('/teams', TeamRouter);
 router.use('/login', UserRouter);
+router.use('/matches', MatchesRouter);
 
 export default router;
