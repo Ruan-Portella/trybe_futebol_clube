34486189c23c5fdf0f3b0f23c6624d483ef1a9d4
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 17:58:40 -0300

    Feat: Adiciona atributo createMatch na interface

--- app/backend/src/Interfaces/IMatchesModels.ts
+++ app/backend/src/Interfaces/IMatchesModels.ts
@@ -4,6 +4,8 @@ export interface IMatchesModel {
   findAll(): Promise<IMatches[]>;
   finishMatchesInProgress(id: number): Promise<boolean>;
   updateMatch(id: number, homeTeam: number, awayTeam: number): Promise<boolean>;
+  createMatch(homeTeamId: number,
+    awayTeamId: number, homeTeamGoals: number, awayTeamGoals: number): Promise<IMatches>;
 }
 
 export default IMatchesModel;
