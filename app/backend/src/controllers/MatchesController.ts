045cdad52c4d608fa9558f230ef4eb38df2ea096
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 14:21:17 -0300

    Feat: passa novo parâmetro para service

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -6,8 +6,9 @@ export default class TeamController {
     private matchesService = new MatchesService(),
   ) {}
 
-  public async findAll(_req: Request, res: Response): Promise<Response> {
-    const matches = await this.matchesService.findAll();
+  public async findAll(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+    const matches = await this.matchesService.findAll(inProgress as string);
     return res.status(200).json(matches);
   }
 }
