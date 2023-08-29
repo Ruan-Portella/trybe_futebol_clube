adee172e308585ed6bb557700170e64165a99e03
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 14:13:28 -0300

    Feat: Adiciona novas propriedades ao IMatches

--- app/backend/src/Interfaces/IMatches.ts
+++ app/backend/src/Interfaces/IMatches.ts
@@ -1,8 +1,16 @@
-export default interface IMatches {
+interface IMatches {
   id: number,
   homeTeamId: number,
   homeTeamGoals: number,
   awayTeamId: number,
   awayTeamGoals: number,
   inProgress: boolean,
+  homeTeam: {
+    teamName: string,
+  }
+  awayTeam: {
+    teamName: string,
+  }
 }
+
+export default IMatches;
