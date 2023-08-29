6c251c1b295dff4af1be25ea9386f819f1975d63
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-29 14:21:01 -0300

    Feat: Coloca condicional para validar o filtro

--- app/backend/src/service/MatchesService.ts
+++ app/backend/src/service/MatchesService.ts
@@ -7,8 +7,17 @@ export default class MatchesService {
     private teamModel: IMatcheModel = new MatcheModel(),
   ) {}
 
-  public async findAll(): Promise<IMatches[]> {
+  public async findAll(query: string | undefined): Promise<IMatches[]> {
     const matches = await this.teamModel.findAll();
-    return matches;
+
+    if (!query) {
+      return matches;
+    }
+
+    if (query === 'true') {
+      return matches.filter((match) => match.inProgress === true);
+    }
+
+    return matches.filter((match) => match.inProgress === false);
   }
 }
