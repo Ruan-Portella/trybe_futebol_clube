2571f051bb2aa2c0cf4f3a81c6bfcf7084797fea
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-28 21:02:43 -0300

    Feat: Cria Interface UserModel

---
+++ app/backend/src/Interfaces/IUsersModel.ts
@@ -0,0 +1,8 @@
+import IUsers from './IUsers';
+
+export interface IUsersModel {
+  // findAll(): Promise<IUsers[]>;
+  findOne(email: string): Promise<IUsers | null>;
+}
+
+export default IUsersModel;
